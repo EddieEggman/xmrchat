166565e3c3dc7536a5c1529ac0930defa9d4cd2f
= <=>
2025-05-17 22:53:29 +0100

    add USD amount to streamer payment modal

--- client/components/payment-modal/StreamerPaymentModal.vue
+++ client/components/payment-modal/StreamerPaymentModal.vue
@@ -19,6 +19,9 @@ const { state: authState, getMe } = useAuthStore();
 const { t } = useI18n();
 const { dayjs } = useDate();
 
+const { xmrToFiat } = useXmrPrice();
+const { money } = useMoney();
+
 const cancelPayment = () => {
   stopPaymentCheck();
   expired.value = false;
@@ -106,7 +109,7 @@ const getTime = () => {
           <template #description>
             <p class="text-[15px] leading-6">
               Please pay a small spam prevention fee of
-              <span class="font-bold">{{ reservedData.amount }} XMR</span> in
+              <span class="font-bold">{{ reservedData.amount }} XMR ({{ money(xmrToFiat(reservedData.amount)) }})</span> in
               the next
               <VueCountdown
                 v-if="reservedData?.reservedUntil"
